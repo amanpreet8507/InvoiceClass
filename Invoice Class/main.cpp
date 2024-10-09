4f4a30572fffba6e62df819e535a7d8bfeebb1cb
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-10-08 17:38:27 -0700

    Initial Commit

---
+++ Invoice Class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Invoice Class
+//
+//  Created by Amanpreeet Sandhu on 2024-10-08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
